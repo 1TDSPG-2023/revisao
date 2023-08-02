27d7c078f0e9b6eb5c7053965598715bfc4abc90
adrian-siv <adrian.ssiv@gmail.com>
2023-08-02 09:32:51 -0300

    Início do exercício 1

---
+++ rm97784/js/exercicio-01.js
@@ -0,0 +1,10 @@
+listaTarefas = [];
+
+function adicionarTarefa() {
+    listaTarefas.push("teste");
+    console.log(listaTarefas);
+}
+
+let lista = document.getElementById("listaDeTarefas");
+
+console.log(lista);
