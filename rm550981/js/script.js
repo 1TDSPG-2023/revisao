0e3df0779d9cc7980c4a5ec56dffb4b3ebcfd594
henricone2005 <rm55091@fiap.com.br>
2023-08-02 09:36:27 -0300

    js normalizado

---
+++ rm550981/js/script.js
@@ -0,0 +1,32 @@
+//let alunos =["joão","maria","henricone"]
+
+//console.log(aluno[0])
+
+//alunos.forEach (aluno=>(
+    //console.log('O nome é ${aluno}')
+//))
+
+//let grupos = [["laura","leticia"] ["henricone","gustavo"] ] 
+
+//console.log(grupos[0][0])
+
+//console.log("\n");
+//Removendo elementos do inicio do array com shift()
+//alunos.shift();
+//alunos.forEach(aluno=>(
+    //console.log(`O nome do aluno é : ${aluno}`)
+//));
+
+//console.log("\n");
+//Alterando elementos no meio do array com splice(posicao-inicial, posicao-final,novos-valores)
+//alunos.splice(1,1,"Adrian");
+//alunos.forEach(aluno=>(
+    //console.log(`O nome do aluno é : ${aluno}`)
+//));
+
+//console.log("\n");
+//Removendo elementos no meio do array com splice(posicao-inicial, posicao-final,não passamos valores nesse caso)
+//alunos.splice(1,1);
+//alunos.forEach(aluno=>(
+    //console.log(`O nome do aluno é : ${aluno}`)
+//));
