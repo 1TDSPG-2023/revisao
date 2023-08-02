074ba13575b668c8a9d1e16dbcfdf588fb1fb9c6
adrian-siv <adrian.ssiv@gmail.com>
2023-08-02 09:16:46 -0300

    adicionadas manipulações feitas na aula

--- rm97784/js/script.js
+++ rm97784/js/script.js
@@ -1,1 +1,27 @@
-console.log("TESTE");
+// //Declarar arrays
+// let alunos = ['João', 'Carlos', 'Maria'];
+
+// //Imprimindo um índice do array
+// console.log(alunos[0]);
+
+// //Utilizando um forEach para imprimir o array
+
+// alunos.forEach(aluno => {
+//     console.log(aluno);
+// });
+
+let alunos = ["João", "Carlos", "Maria"];
+
+alunos.push("Ana");
+
+// alunos.forEach(aluno => {
+//     console.log(`${aluno} é aluno(a) da FIAP.`)
+// })
+
+alunos.sort()
+
+console.log(alunos);
+
+alunos.splice(1,2,"Adrian");
+
+console.log(alunos);
