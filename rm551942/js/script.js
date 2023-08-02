e960f1e997b5aab8ae5d36929f0770055c3f02bb
BrunoDuller <rm551942@fiap.com.br>
2023-08-02 10:13:58 -0300

    Exercicio Array.

--- rm551942/js/script.js
+++ rm551942/js/script.js
@@ -19,64 +19,80 @@
 // console.log(grupos[0][0])
 // console.log(alunos[0])
 
-//Declarar arrays de alunos
-let alunos = ["João","Carlos","Maria"];
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-))
+// //Declarar arrays de alunos
+// let alunos = ["João","Carlos","Maria"];
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ))
 
-//Adicionando um novo aluno ao final do array com push();
-console.log("\n");
-alunos.push('Ana');
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-));
+// //Adicionando um novo aluno ao final do array com push();
+// console.log("\n");
+// alunos.push('Ana');
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
 
-console.log("\n");
-//Utilizando o método sort para organizar o array em ordem alfabética.
-alunos.sort()
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-));
+// console.log("\n");
+// //Utilizando o método sort para organizar o array em ordem alfabética.
+// alunos.sort()
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
 
-console.log("\n");
-//Utilizando o reverse para reverter a ordem que se apresenta
-alunos.reverse();
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-));
+// console.log("\n");
+// //Utilizando o reverse para reverter a ordem que se apresenta
+// alunos.reverse();
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
 
-console.log("\n");
-//Utilizando o pop() para remover o último elemento do array.
-alunos.pop();
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-));
+// console.log("\n");
+// //Utilizando o pop() para remover o último elemento do array.
+// alunos.pop();
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
 
-console.log("\n");
-//Adicionando elementos ao inicio do array com unshift()
-alunos.unshift("Jorge");
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-));
+// console.log("\n");
+// //Adicionando elementos ao inicio do array com unshift()
+// alunos.unshift("Jorge");
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
 
-console.log("\n");
-//Removendo elementos do inicio do array com shift()
-alunos.shift();
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-));
+// console.log("\n");
+// //Removendo elementos do inicio do array com shift()
+// alunos.shift();
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
 
-console.log("\n");
-//Alterando elementos no meio do array com splice(posicao-inicial, posicao-final,novos-valores)
-alunos.splice(1,1,"Adrian");
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
-));
 
-console.log("\n");
-//Removendo elementos no meio do array com splice(posicao-inicial, posicao-final, não passamos valores nesse caso)
-alunos.splice(1,1);
-alunos.forEach(aluno=>(
-    console.log(`O nome do aluno é : ${aluno}`)
+// console.log("\n");
+// //Alterando elementos no meio do array com splice(posicao-inicial, posicao-final,novos-valores)
+// alunos.splice(1,1,"Adrian");
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
+
+// console.log("\n");
+// //Removendo elementos no meio do array com splice(posicao-inicial, posicao-final,não passamos valores nesse caso)
+// alunos.splice(1,1);
+// alunos.forEach(aluno=>(
+//     console.log(`O nome do aluno é : ${aluno}`)
+// ));
+
+const cursos = [
+    {"nome" : "HTML-5", "duracao":"3 meses"},
+    {"nome" : "CSS-3", "duracao":"4 meses"},
+    {"nome" : "Javascript", "duracao":"5 meses"},
+    {"nome" : "React", "duracao":"5 meses"}
+]
+
+console.log(cursos);
+
+//Imprimindo cada um dos objetos e apenas uma das propriedades.
+const nomeCursos = cursos.map( (curso,key) =>(
+    
+    console.log(`${key+1} - ${curso.nome} - ${curso["duracao"]}` )
 ));
