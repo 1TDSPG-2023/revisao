f48538ccfc82a3121efe01f6a4fd4f616340d73d
adrian-siv <adrian.ssiv@gmail.com>
2023-08-02 19:56:36 -0300

    Adicionadas funções de adicionar e retirar tarefas

--- rm97784/js/exercicio-01.js
+++ rm97784/js/exercicio-01.js
@@ -1,10 +1,44 @@
-listaTarefas = [];
+let listaTarefas = [];
+let listaImpressa = document.getElementById("listaDeTarefas");
+const entradaDeTarefa = document.getElementById("inputTarefa");
+const botaoAdicionar = document.getElementById("botaoAdicionarTarefa");
+const formularioTarefas = document.getElementById("formularioTarefas");
 
-function adicionarTarefa() {
-    listaTarefas.push("teste");
-    console.log(listaTarefas);
+
+formularioTarefas.addEventListener("submit", prevenirComportamentoPadrao);
+botaoAdicionar.addEventListener("click",adicionarTarefa);
+
+function prevenirComportamentoPadrao(evt) {
+    evt.preventDefault();
 }
 
-let lista = document.getElementById("listaDeTarefas");
+function imprimirTarefa() {
+    var novaTarefa = document.createElement("li");
+    novaTarefa.textContent = `${entradaDeTarefa.value}`;
+    console.log(novaTarefa.textContent);
+    listaImpressa.appendChild(novaTarefa);
+}
 
-console.log(lista);
+function adicionarTarefa() {
+    listaTarefas.push(entradaDeTarefa.value);
+    atualizarLista();
+    entradaDeTarefa.value = "";
+    entradaDeTarefa.focus();
+}
+
+function atualizarLista() {
+    listaImpressa.innerHTML = "";
+    listaTarefas.forEach((tarefa, i) => {
+        let novaTarefa = document.createElement("li");
+        novaTarefa.textContent = `${tarefa}`;
+        listaImpressa.appendChild(novaTarefa);
+
+        let botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "Excluir tarefa";
+        botaoExcluir.addEventListener("click", function() {
+            listaTarefas.splice(i, 1);
+            atualizarLista();
+        });
+        novaTarefa.appendChild(botaoExcluir);
+    })
+}
