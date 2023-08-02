2f965951658996ea1517a50895124fbd8241a79c
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2023-08-02 09:59:40 -0300

    Inicialização do Exercicio 1

---
+++ exercicio-01-arrays/js/script.js
@@ -0,0 +1,8 @@
+let info = document.getElementById("enviar");
+let tarefas = [''];
+
+array.forEach(info => {
+    tarefas.push(info)
+    console.log(info)
+});
+
---
+++ rm98251/js/script.js
@@ -0,0 +1,9 @@
+const cursos = [
+    {'nome' : 'HTML5', 'duracao' : '3 meses'},
+    {'nome' : 'CSS3', 'duracao' : '4 meses'},
+    {'nome' : 'Javascript', 'duracao' : '5 meses'},
+]
+
+console.log(cursos);
+
+const nomeCursos = cursos.map(cursos => cursos.nome)
