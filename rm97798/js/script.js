0f59913f8cc77b5438395860c66f3fb0d974ed78
esterdsilva <rm97798@fiap.com.br>
2023-08-02 08:52:46 -0300

    criando array

--- rm97798/js/script.js
+++ rm97798/js/script.js
@@ -1,5 +1,15 @@
 //declarar arrays
-let aluno = ["João", "Carlos", "Maria"];
+//let alunos = ["João", "Carlos", "Maria"];
 
 //Imprimindo um indice do array
-console.log(aluno[0])
+//console.log(alunos[0])
+
+//utilizando um forEach para imprimir o array
+//alunos.forEach(aluno=>(
+    console.log(`O nome é ${aluno}`)
+//))
+
+//criando um array de array de alunos e suas notas
+let grupos = [["Laura", "Leticia"], ["Pedro", "Gustavo"]]
+//Imprimindo o primeiro array
+console.log(grupos[0][0])
