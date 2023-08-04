c0355bf84945413e73bfec8732b4d5f84865b5c7
mthaugusto <mthaugusto94@icloud.com>
2023-08-04 12:08:04 -0300

    Alterações criando elementos

--- rm551616/js/script.js
+++ rm551616/js/script.js
@@ -94,3 +94,60 @@ alunos.splice(1,1);
 alunos.forEach(aluno=>(
     console.log(`O nome do aluno é: ${aluno}`)
 ));
+
+// UTILIZANDO MAP 
+
+const cursos = [
+    {"nome" : "HTML-5", "duracao":"3 meses"},
+    {"nome" : "CSS-3", "duracao":"4 meses"},
+    {"nome" : "Javascript", "duracao":"5 meses"},
+    {"nome" : "React", "duracao":"5 meses"}
+]
+
+// console.log(cursos);
+
+// //Imprimindo cada um dos objetos e apenas uma das propriedades.
+// const nomeCursos = cursos.map((curso,key) =>(
+//     console.log(`${key+1} - ${curso.nome} - ${curso["duracao"]}` )
+// ));
+
+// Criando a UL
+const ul = document.createElement("ul"); // aqui o documento ainda não existe, ele apenas foi criado na memória. 
+
+//Adicionando a ul a div com id box-cursos
+const boxCursos = document.getElementById("box-cursos");
+
+boxCursos.appendChild(ul);
+
+// cursos.forEach(curso=>{
+    //     const li = document.createElement("li");
+    //     li.textContent = `${curso.nome} - ${curso.duracao}`;
+    //     ul.appendChild(li);
+    // });
+    
+    
+let indiceClassCurso = 0;
+cursos.forEach(curso=>{
+    indiceClassCurso++;
+    const li = document.createElement("li");
+    li.id = `curso-${indiceClassCurso}`;
+    li.textContent = `${curso.nome} - ${curso.duracao}`;
+    ul.appendChild(li);
+});
+
+
+function cadastrar(nomeCurso,duracaoCurso){
+   
+    //Recebendo os valores que o usuário digitou
+    const novoCurso = {"nome":nomeCurso,"duracao":duracaoCurso};
+    //Adicionando o novo curso ao array de cursos
+    cursos.push(novoCurso);
+    //Adicionando o novo curso a lista de cursos
+    const li = document.createElement("li");
+    //Adicionando o id ao li
+    li.id = `curso-${cursos.length}`;
+    //Adicionando o texto ao li
+    li.textContent = `${nomeCurso} - ${duracaoCurso}`;
+    //Adicionando o li a ul
+    ul.appendChild(li);
+}
