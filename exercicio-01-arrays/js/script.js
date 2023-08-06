ac385c4bfd6b49f3ad37b086b71156b4599dfa97
Marcelo Dias <marcelodias.desenvolvedor@gmail.com>
2023-08-06 15:14:17 -0300

    Opção de excluir + css

--- exercicio-01-arrays/js/script.js
+++ exercicio-01-arrays/js/script.js
@@ -1,18 +1,34 @@
-let listaTarefas = []
-let listaImpressaTarefas = document.getElementById("lista-tarefas")
-let nomeTarefa = document.getElementById("nome-tarefa")
-let botaoAdicionar = document.getElementById("btn-adicionar")
-
-botaoAdicionar.addEventListener("click", adicionarTarefa)
+let listaTarefas = [];
+const botaoAdicionar = document.getElementById("btn-adicionar");
 
 function adicionarTarefa() {
-    listaTarefas.push(nomeTarefa)
-    imprimirTarefa()
+    const nomeTarefa = document.getElementById("nome-tarefa");
+    listaTarefas.push(nomeTarefa.value);
+
+    const li = document.createElement("li");
+    li.textContent = nomeTarefa.value;
+    li.id = `tarefa-${listaTarefas.length}`;
+
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = "x";
+    botaoExcluir.id = `botao-${listaTarefas.length}`;
+
+    const ul = document.getElementById("lista-tarefas");
+    ul.appendChild(li);
+    li.appendChild(botaoExcluir);
+
+    botaoExcluir.addEventListener("click",(evt)=>{
+        listaTarefas.forEach((tarefa, index)=>{
+            if(tarefa === evt.target.parentNode.textContent.split("-")[0]){
+                listaTarefas.splice(index,1);
+                ul.removeChild(evt.target.parentNode);
+            }
+        })
+        ul.removeChild(evt.target.parentNode);
+    });
+    
+    nomeTarefa.focus()
 }
 
-function imprimirTarefa() {
-    let novaTarefa = document.createElement("li")
-    novaTarefa.textContent = nomeTarefa.value
-    listaImpressaTarefas.appendChild(novaTarefa)
-}
+botaoAdicionar.addEventListener("click", adicionarTarefa)
 
