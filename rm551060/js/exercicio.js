5cf30a31c8a8435bee1bbd358137f5f3ece346af
Nick-Rodrigues <RM551060@fiap.com.br>
2023-08-08 15:47:47 -0300

    Botão para excluir tarefas feito.

--- rm551060/js/exercicio.js
+++ rm551060/js/exercicio.js
@@ -1,19 +1,10 @@
-let tarefas = [];
+const tarefas = [];
 
 const ul = document.createElement("ul");
 
 const boxCursos = document.getElementById("box-tarefas");
 
 boxCursos.appendChild(ul);
-
-tarefas.forEach(tarefas=>{
-
-    const li = document.createElement("li");
-    li.id = Tarefa-`${tarefas.length}`;
-    li.textContent = `${tarefas.nome}`;
-    ul.appendChild(li);
-
-});
 
 function cadastrar(){ 
 
@@ -22,10 +13,25 @@ function cadastrar(){
 
     tarefas.push(novaTarefa);
     const li = document.createElement("li");
+    const btnExcluir = document.createElement("button");
+    btnExcluir.textContent = "x";
+    btnExcluir.setAttribute("class","btnExcluir");
     li.id = tarefas-`${tarefas.length}`;
     li.textContent = `${nomeTarefa}`;
     ul.appendChild(li);
+    li.appendChild(btnExcluir);
     li.setAttribute("class","tarefas");
 
+    btnExcluir.addEventListener("click",(evt)=>{
+        tarefas.forEach((tarefas,index)=>{
+            if(tarefas.nome === evt.target.parentNode.textContent.split("-")[0]){
+                tarefas.splice(index,1);
+                ul.removeChild(evt.target.parentNode);
+            }
+        });
+
+        ul.removeChild(evt.target.parentNode);
+    });
 }
+
 
