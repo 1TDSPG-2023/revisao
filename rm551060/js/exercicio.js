5025d3c327faf93609c5cb45e0094805af3136a9
Nick-Rodrigues <RM551060@fiap.com.br>
2023-08-08 15:38:15 -0300

    Adicionar tarefas feito.

--- rm551060/js/exercicio.js
+++ rm551060/js/exercicio.js
@@ -1,3 +1,31 @@
 let tarefas = [];
 
+const ul = document.createElement("ul");
+
+const boxCursos = document.getElementById("box-tarefas");
+
+boxCursos.appendChild(ul);
+
+tarefas.forEach(tarefas=>{
+
+    const li = document.createElement("li");
+    li.id = Tarefa-`${tarefas.length}`;
+    li.textContent = `${tarefas.nome}`;
+    ul.appendChild(li);
+
+});
+
+function cadastrar(){ 
+
+    const nomeTarefa = document.getElementById("idTarefa").value;
+    const novaTarefa = {"nome":nomeTarefa};
+
+    tarefas.push(novaTarefa);
+    const li = document.createElement("li");
+    li.id = tarefas-`${tarefas.length}`;
+    li.textContent = `${nomeTarefa}`;
+    ul.appendChild(li);
+    li.setAttribute("class","tarefas");
+
+}
 
