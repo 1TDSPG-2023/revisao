a8acb95987ca737e7ab577421f37ebb3f0b2ae7a
pedroantuness <RM551409@fiap.com.br>
2023-08-08 15:46:51 -0300

    Finalizado exercicio array-01

--- rm551409/js/exercicio-01-array.js
+++ rm551409/js/exercicio-01-array.js
@@ -1,1 +1,37 @@
-let tarefas = [];
+const tarefas = [];
+
+const ul = document.createElement("ul");
+const boxCursos = document.getElementById("box-tarefas");
+boxCursos.appendChild(ul);
+
+function cadastrar(){ 
+    
+    const nomeTarefa = document.getElementById("idTarefa").value;
+    const novaTarefa = {"nome":nomeTarefa};
+
+    tarefas.push(novaTarefa);
+    const li = document.createElement("li");
+    const btnExcluir = document.createElement("button");
+    btnExcluir.textContent = "x";
+    btnExcluir.setAttribute("class","btnExcluir");
+    li.id = `tarefas-${tarefas.length}`;
+    li.textContent = `${nomeTarefa}`;
+    ul.appendChild(li);
+    li.setAttribute("class","tarefas");
+    li.appendChild(btnExcluir);
+    btnExcluir.addEventListener("click",(evt)=>{
+
+        tarefas.forEach((tarefas,index)=>{
+            if(tarefas.nome === evt.target.parentNode.textContent.split("-")[0]){
+                tarefas.splice(index,1);
+                ul.removeChild(evt.target.parentNode);
+            }
+        });
+
+        ul.removeChild(evt.target.parentNode);
+
+    });
+   
+}
+
+console.log(tarefas);
