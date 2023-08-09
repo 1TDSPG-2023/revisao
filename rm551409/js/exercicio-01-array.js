7264a0d3f974af01b1e1f34e7459de7c0108aed5
pedroantuness <rm551409@fiap.com.br>
2023-08-09 08:49:38 -0300

    ultima atualizaçao exercicio array

--- rm551409/js/exercicio-01-array.js
+++ rm551409/js/exercicio-01-array.js
@@ -4,20 +4,23 @@ const ul = document.createElement("ul");
 const boxTarefas = document.getElementById("tarefas");
 boxTarefas.appendChild(ul);
 
-function cadastrar(){ 
+function cadastrarTarefa(){ 
     
     const nomeTarefa = document.getElementById("idTarefa").value;
     const novaTarefa = {"Tarefa":nomeTarefa};
 
     tarefas.push(novaTarefa);
     const li = document.createElement("li");
+
     const btnExcluir = document.createElement("button");
     btnExcluir.textContent = "x";
     btnExcluir.setAttribute("class","btnExcluir");
+
     li.id = `tarefas-${tarefas.length}`;
     li.textContent = `${nomeTarefa}`;
     ul.appendChild(li);
     li.setAttribute("class","tarefas");
+    
     li.appendChild(btnExcluir);
     btnExcluir.addEventListener("click",(evt)=>{
 
