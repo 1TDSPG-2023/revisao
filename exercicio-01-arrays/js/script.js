c8c5b8d81c836aad0ef2d58a4cbf2ec2fc9e54bc
jaciteixeira <rr99627@fiap.com.br>
2023-08-09 08:30:41 -0300

    ultima atualização

--- exercicio-01-arrays/js/script.js
+++ exercicio-01-arrays/js/script.js
@@ -1,42 +1,41 @@
-let Tarefas = ['Tarefa1'];
-let listaImpressa = document.getElementById("listTarefas");
-const inputTarefa = document.getElementById("inputTarefas");
-const botaoAddTarefa = document.getElementById("bntAddTarefas");
-const formularioTarefas = document.getElementById("formularioTarefa");
+let listaTarefas = [];
+let listaImpressa = document.getElementById("listaDeTarefas");
+const inputDeTarefa = document.getElementById("inputTarefa");
+const botaoAdicionar = document.getElementById("botaoAdicionarTarefa");
 
-function adicionarTarefa(){
-    Tarefas.push(inputTarefa.value);
-    atualizarLista();
-    inputTarefa.value = '';
-    console.log(Tarefas)
-}
 
-function mostrarLista(){
-    let novaTarefa = document.createElement("li");
-    novaTarefa.textContent = `${inputTarefa.value}`;
+botaoAdicionar.addEventListener("click",adicionarTarefa);
+
+
+
+function imprimirTarefa() {
+    var novaTarefa = document.createElement("li");
+    novaTarefa.textContent = `${inputDeTarefa.value}`;
     console.log(novaTarefa.textContent);
-    listaDeTarefas.appendChild(novaTarefa);
+    listaImpressa.appendChild(novaTarefa);
 }
 
-function atualizarLista(){
-    listaImpressa.innerHTML = '';
-    listaDeTarefas.forEach((adicionaTarefa,index) => {
-        const novaTarefa = document.createElement("li")
-        novaTarefa.textContent=`${adicionaTarefa}`;
+function adicionarTarefa() {
+    listaTarefas.push(inputDeTarefa.value);
+    atualizarLista();
+    inputDeTarefa.value = "";
+    inputDeTarefa.focus();
+    console.log(listaTarefas);
+}
+
+function atualizarLista() {
+    listaImpressa.innerHTML = "";
+    listaTarefas.forEach((tarefaAdicionada, index) => {
+        let novaTarefa = document.createElement("li");
+        novaTarefa.textContent = `${tarefaAdicionada}`;
         listaImpressa.appendChild(novaTarefa);
 
-        const botaoDeletar = document.createElement("button");
-        botaoDeletar.textContent = 'Excluir';
-        botaoDeletar.addEventListener('click',function(){
-            deletarTarefa(index);
+        let botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "Excluir";
+        botaoExcluir.addEventListener("click", function() {
+            listaTarefas.splice(index, 1);
+            atualizarLista();
         });
-    });
-    novaTarefa.appendChild(botaoDeletar);
+        novaTarefa.appendChild(botaoExcluir);
+    })
 }
-
-function deletarTarefa(index){
-    Tarefas.splice(index,1);
-    atualizarLista();
-}
-
-console.log(Tarefas);
