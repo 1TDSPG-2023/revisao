c903826b94c913b4ef16ef976abe0d46c31d089d
PedroPaterno <rm99726@fiap.com.br>
2023-08-08 21:45:47 -0300

    javascript completo

---
+++ rm99726/js/exercicio-01-arrays.js
@@ -0,0 +1,37 @@
+const listaTarefas = [];
+const botaoAdicionar = document.getElementById("btnadicionartarefas");
+
+function adicionarTarefa() {
+    const nomeTarefaInput = document.getElementById("nometarefas");
+    const nomeTarefa = nomeTarefaInput.value;
+    if (nomeTarefa.trim() !== '') {
+        listaTarefas.push(nomeTarefa);
+        atualizarLista();
+    }
+}
+
+function atualizarLista() {
+    const ul = document.getElementById("listatarefas");
+    ul.innerHTML = ''; // Limpa a lista atual
+
+    for (let i = 0; i < listaTarefas.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = listaTarefas[i];
+
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "X";
+
+        li.appendChild(botaoExcluir);
+        ul.appendChild(li);
+
+        botaoExcluir.addEventListener("click", () => {
+            listaTarefas.splice(i, 1);
+            atualizarLista();
+        });
+    }
+
+    document.getElementById("nometarefas").value = '';
+    document.getElementById("nometarefas").focus();
+}
+
+botaoAdicionar.addEventListener("click", adicionarTarefa);
