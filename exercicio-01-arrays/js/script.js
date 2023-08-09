943f8908e70f8a33663bcd6dd2d4ccb5fdd46f7e
jaciteixeira <rm99627@fiap.com.br>
2023-08-08 23:27:49 -0300

    ultima atualização

--- exercicio-01-arrays/js/script.js
+++ exercicio-01-arrays/js/script.js
@@ -1,9 +1,42 @@
-const btn = document.getElementById("meu-btn");
+let Tarefas = ['Tarefa1'];
+let listaImpressa = document.getElementById("listTarefas");
+const inputTarefa = document.getElementById("inputTarefas");
+const botaoAddTarefa = document.getElementById("bntAddTarefas");
+const formularioTarefas = document.getElementById("formularioTarefa");
 
-console.log(btn.textContent);
-btn.addEventListener();
+function adicionarTarefa(){
+    Tarefas.push(inputTarefa.value);
+    atualizarLista();
+    inputTarefa.value = '';
+    console.log(Tarefas)
+}
 
-let alunos = [];
-alunos.forEach(nome => {
-    
-});
+function mostrarLista(){
+    let novaTarefa = document.createElement("li");
+    novaTarefa.textContent = `${inputTarefa.value}`;
+    console.log(novaTarefa.textContent);
+    listaDeTarefas.appendChild(novaTarefa);
+}
+
+function atualizarLista(){
+    listaImpressa.innerHTML = '';
+    listaDeTarefas.forEach((adicionaTarefa,index) => {
+        const novaTarefa = document.createElement("li")
+        novaTarefa.textContent=`${adicionaTarefa}`;
+        listaImpressa.appendChild(novaTarefa);
+
+        const botaoDeletar = document.createElement("button");
+        botaoDeletar.textContent = 'Excluir';
+        botaoDeletar.addEventListener('click',function(){
+            deletarTarefa(index);
+        });
+    });
+    novaTarefa.appendChild(botaoDeletar);
+}
+
+function deletarTarefa(index){
+    Tarefas.splice(index,1);
+    atualizarLista();
+}
+
+console.log(Tarefas);
