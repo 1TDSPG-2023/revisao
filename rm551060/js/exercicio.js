71ee13510fd9f7256edb42491bc9a0fed670a562
Nick-Rodrigues <RM551060@fiap.com.br>
2023-08-14 18:24:17 -0300

    Correção exercício aula anterior.

--- rm551060/js/exercicio.js
+++ rm551060/js/exercicio.js
@@ -1,31 +1,46 @@
-const tarefas = [];
+//Criar a lista de Tarefas array
+let listaTarefasArray = [];
 
-const ul = document.createElement("ul");
-const boxTarefas = document.getElementById("box-tarefas");
-boxTarefas.appendChild(ul);
+const botaoAddtarefa = document.getElementById("btnAddTarefa");
 
-function adicionar(){ 
+botaoAddtarefa.addEventListener("click", (evento)=>{
 
-    const nomeTarefa = document.getElementById("idTarefa").value;
-    const novaTarefa = {"nome":nomeTarefa};
+    evento.preventDefault();
 
-    //Push tarefa
-    tarefas.push(novaTarefa);
-    const li = document.createElement("li");
-    const btnExcluir = document.createElement("button");
-    btnExcluir.textContent = "x";
-    btnExcluir.setAttribute("class","btnExcluir");
-    li.id = tarefas-`${tarefas.length}`;
-    li.textContent = `${nomeTarefa}`;
-    ul.appendChild(li);
-    li.appendChild(btnExcluir);
-    li.setAttribute("class","tarefas");
+    const inputTarefa = document.getElementById("idTarefa");
 
-    //Btn excluir
-    btnExcluir.addEventListener("click",(evt)=>{
-        ul.removeChild(evt.target.parentNode);
-    });
-    console.log(tarefas)
-}
+    listaTarefasArray.push(inputTarefa.value);
 
+    const listaTarefasUL = document.getElementById("lista-tarefas");
+
+    let li = document.createElement("li");
+
+    li.textContent = inputTarefa.value;
+
+    listaTarefasUL.appendChild(li);
+
+    let botaoExcluir = document.createElement("button");
+
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+
+    botaoExcluir.addEventListener("click",(evt)=>{
+
+        let conteudoDoLi = evt.target.parentNode.textContent.split(" ");
+        console.log(conteudoDoLi);
+
+        let index = listaTarefasArray.indexOf(conteudoDoLi[0]);
+
+        listaTarefasArray.splice(index,1);
+
+        evt.target.parentNode.remove();
+
+        console.log(listaTarefasArray);
+
+    })
+
+    inputTarefa.value = "";
+    console.log(listaTarefasArray);
+});
 
