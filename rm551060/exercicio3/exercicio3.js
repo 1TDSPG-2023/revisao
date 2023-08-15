ddc786006321882482ce0c1f2e0a6a92d4190796
Nick-Rodrigues <RM551060@fiap.com.br>
2023-08-14 21:56:03 -0300

    Elementos sendo adicionados em uma string.

--- rm551060/exercicio3/exercicio3.js
+++ rm551060/exercicio3/exercicio3.js
@@ -1,3 +1,44 @@
-let tarefas = [
-    {'descricao' : 'limpar' , 'autor' : 'Nicolas', 'departamento' : 'limpeza', 'importancia' : '1'}
-]
+let tarefas = [];
+
+const botaoAddtarefa = document.getElementById("btnAddTarefa");
+
+botaoAddtarefa.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+
+    const inputTarefa = document.getElementById('idTarefa').value;
+    const inputDescricao = document.getElementById('idDescricao').value;
+    const inputAutor = document.getElementById('idAutor').value;
+    const inputDepartamento = document.getElementById('idDepartamento').value;
+    const inputImportancia = document.getElementById('idImportancia').value;
+
+    const tarefa = {
+        inputTarefa,
+        inputDescricao,
+        inputAutor,
+        inputDepartamento,
+        inputImportancia,
+    };
+
+    tarefas.push(tarefa);
+
+    const tabelaTarefas = document.getElementById("tabela");
+    let td = document.createElement("td");
+    td.textContent = inputTarefa + inputDescricao + inputAutor + inputDepartamento + inputImportancia;
+    tabelaTarefas.appendChild(td);
+    let botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = " x ";
+    td.appendChild(botaoExcluir);
+
+    botaoExcluir.addEventListener("click",(evt)=>{
+        let conteudoDoTr = evt.target.parentNode.textContent.split(" ");
+        console.log(conteudoDoTr);
+        let index = tarefas.indexOf(conteudoDoTr[0]);
+        tarefas.splice(index,1);
+        evt.target.parentNode.remove();
+        console.log(tarefas);
+
+     })
+
+    // inputTarefa.value = "";
+    console.log(tarefas);
+});
