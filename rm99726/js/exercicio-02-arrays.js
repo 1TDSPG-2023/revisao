c7880a74dc2a94c5eba1d043792fb7a80f25638b
PedroPaterno <rm99726@fiap.com.br>
2023-08-15 20:15:38 -0300

    Ex 2 e 3 finalizados

---
+++ rm99726/js/exercicio-02-arrays.js
@@ -0,0 +1,18 @@
+const salarios = [1000,1500,2000,2500,3000,3500,4000,4500,5000,5500]
+
+const salariosMultiplicados = salarios.map(salario => {
+
+    if (salario <= 2000) {
+        return salario * 1.15;
+
+    }else {
+        return salario * 1.10;
+    }
+}); 
+
+const salarioMaior2500 = salariosMultiplicados.filter(salario => salario > 2500);
+
+const somaSalariosMaior2500 = salarioMaior2500.reduce((valorsoma, salario) => valorsoma + salario, 0);
+
+
+
---
+++ rm99726/js/exercicio-03-arrays.js
@@ -0,0 +1,80 @@
+const listaTarefas = [];
+
+const form = document.getElementById("formularioTarefas");
+const tabela = document.getElementById("listaTarefas");
+const listaPorImportancia = document.getElementById("listaPorImportancia"); // Adicionado
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const descricao = document.getElementById("descricao").value;
+    const autor = document.getElementById("autor").value;
+    const departamento = document.getElementById("departamento").value;
+    const importancia = document.getElementById("importancia").value;
+
+    const novaTarefa = {
+        descricao: descricao,
+        autor: autor,
+        departamento: departamento,
+        importancia: importancia,
+    };
+
+    listaTarefas.push(novaTarefa);
+    atualizarTabela();
+    form.reset();
+});
+
+function criarLinhaTabela(tarefa, index) {
+    return `
+        <tr>
+            <td>${tarefa.descricao}</td>
+            <td>${tarefa.autor}</td>
+            <td>${tarefa.departamento}</td>
+            <td>${tarefa.importancia}</td>
+            <td>
+                <button class="excluir" data-index="${index}">Excluir</button>
+            </td>
+        </tr>
+    `;
+}
+
+function atualizarTabela() {
+    tabela.innerHTML = '';
+
+    listaTarefas.forEach((tarefa, index) => {
+        tabela.innerHTML += criarLinhaTabela(tarefa, index);
+    });
+
+    const botoesExcluir = tabela.querySelectorAll(".excluir");
+    botoesExcluir.forEach((botao) => {
+        botao.addEventListener("click", function () {
+            const index = parseInt(botao.getAttribute("data-index"));
+            listaTarefas.splice(index, 1);
+            atualizarTabela();
+        });
+    });
+}
+
+
+function criarListaPorImportancia() {
+    listaPorImportancia.innerHTML = '';
+
+    let tarefasPorImportancia = listaTarefas.sort((a, b) => {
+        const ordem = {"Alta" : 1, "Média" : 2, "Baixa" : 3}
+        return ordem[a.importancia] - ordem[b.importancia]
+    })
+    
+
+    tarefasPorImportancia.forEach((obj) => {
+        const li = document.createElement("li");
+        li.textContent = obj.descricao;
+        listaPorImportancia.appendChild(li);
+        console.log(tarefasPorImportancia, "pedro")
+        console.log(descricao, "descricao")
+    });
+}
+console.log(listaTarefas)
+
+
+const btnListarPorImportancia = document.getElementById("btnListaPorImportancia");
+btnListarPorImportancia.addEventListener("click", criarListaPorImportancia);
