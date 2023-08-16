8a3905bd23b12afe6596d4171c5ced6e55b64ce3
renataBelila <renata.belila1@gmail.com>
2023-08-16 08:08:27 -0300

    att

--- exercício_01_array/js/lista.js
+++ exercício_01_array/js/lista.js
@@ -1,14 +1,15 @@
-const task = []
+const salarios = [1000,1500,2000,2500,3000,3500,4000,4500,5000,5500]
 
-    
-    let novaTarefa = document.getElementById("barra_tarefas").value;
-    let novoAutor = document.getElementById("autorInput").value
-    let novoDept = document.getElementById("deptInput").value
-    let novoImportancia = document.getElementById("importanciaSelect").value
-    let novaDescricao = document.getElementById("descriInput").value
-    let novoValor = document.getElementById("novoValor").value
-    task.push({tarefa: novaTarefa, Autor: novoAutor, Dept: novoDept, importancia: novoImportancia,
-    Descricao: novaDescricao, valor: null});
+const salariosMultiplicados = salarios.map(salario => {
 
+    if (salario <= 2000) {
+        return salario * 1.15;
 
-    
+    }else {
+        return salario * 1.10;
+    }
+}); 
+
+const salarioMaior2500 = salariosMultiplicados.filter(salario => salario > 2500);
+
+const somaSalariosMaior2500 = salarioMaior2500.reduce((valorsoma, salario) => valorsoma + salario, 0);
